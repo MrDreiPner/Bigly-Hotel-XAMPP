ad59b66e29e10b73296015c81a5dffc8799ccf65
M.Porubsky <porubsky1995@hotmail.com>
2021-11-26 20:07:10 +0100

    header -> head
    was a dumb name tbh

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include "header.php"; ?>
+<?php include "head.php"; ?>
 <body>
     <?php
         session_start();
--- registrierung.php
+++ registrierung.php
@@ -1,4 +1,4 @@
-<?php include "header.php"; ?>
+<?php include "head.php"; ?>
 <body>
     <?php
         include "nav.php";
