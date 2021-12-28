8d1aaf95f0b9a67387977b058d89664104313674
M.Porubsky <porubsky1995@hotmail.com>
2021-12-28 22:17:35 +0100

    DB 2

--- DB/bighotel_db.sql
+++ DB/bighotel_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 28, 2021 at 09:56 PM
+-- Generation Time: Dec 28, 2021 at 10:16 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 8.1.1
 
@@ -20,6 +20,25 @@ SET time_zone = "+00:00";
 --
 -- Database: `bighotel_db`
 --
+CREATE DATABASE IF NOT EXISTS `bighotel_db` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `bighotel_db`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
+
+CREATE TABLE IF NOT EXISTS `tickets` (
+  `ticketID` int(255) NOT NULL,
+  `text_guest` text NOT NULL,
+  `image_path` varchar(255) NOT NULL,
+  `text_service` text NOT NULL,
+  `resolved` int(4) NOT NULL,
+  `userID` int(255) NOT NULL,
+  PRIMARY KEY (`ticketID`),
+  KEY `userID` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -27,7 +46,7 @@ SET time_zone = "+00:00";
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(80) NOT NULL,
@@ -35,18 +54,19 @@ CREATE TABLE `user` (
   `vorname` varchar(50) NOT NULL,
   `nachname` varchar(50) NOT NULL,
   `room` int(255) NOT NULL,
-  `userID` int(255) NOT NULL
+  `userID` int(255) NOT NULL,
+  PRIMARY KEY (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Indexes for dumped tables
+-- Constraints for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Constraints for table `tickets`
 --
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`userID`);
+ALTER TABLE `tickets`
+  ADD CONSTRAINT `tickets_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `user` (`userID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
