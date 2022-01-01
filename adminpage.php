b33ddd7858a80f56936de4b6b54f1b13036a3dab
PatFalt <81306186+PatFalt@users.noreply.github.com>
2022-01-01 23:56:34 +0100

    Create adminpage.php

---
+++ adminpage.php
@@ -0,0 +1,11 @@
+<?php include "head.php" ?>
+<body>
+    <h1>Hello Admin!</h1>
+    <a href="registrierung.php">Add User</a>
+    <a href="manageUser.php">Manage Users</a>
+    <a href="addNews.php">Add News</a>
+    <a href="manageNews.php">Manage News</a>
+    <a href="ticketVerwaltung.php">Ticket Verwaltung</a>
+
+</body>
+</html>
