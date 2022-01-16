479f0348f173baf19f5a62a2c83e722ac95ae476
PatFalt <81306186+PatFalt@users.noreply.github.com>
2022-01-16 23:32:17 +0100

    include printnews in index

--- index.php
+++ index.php
@@ -20,30 +20,6 @@
         <h3>News Feed</h3>
     </div>
     <br>
-    <?php
-        //fetched die active gesetzten News aus der Datenbank
-        $sql = 'select headline, imgpath, content, date from news 
-                where active = true
-                order by date desc';
-        $stmt = $db_obj->prepare($sql);
-        if ($stmt===false){
-            echo($db_obj->error);
-            echo "fail";
-        }
-        $stmt->execute();
-        $stmt->bind_result($headline, $imgpath, $content, $date);
-    ?>
-    <ul>
-        <?php 
-            //Printet die News Beiträge
-            while($stmt->fetch()){
-                echo "<li><p><h4>". $headline. "</h4>". $date ."<br>"
-                 . $content . "<br><img src='". $imgpath ."' alt ='". $headline ."'>
-                 </p>
-                </li>";
-            }
-            $stmt->close(); $db_obj->close();
-        ?>
-    </ul>
+    <?php include "printNews.php"; ?>
 </body>
 </html>
