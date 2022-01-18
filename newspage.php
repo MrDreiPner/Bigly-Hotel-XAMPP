10b7e259631a715dea55cad9ed7abe024d148bda
M.Porubsky <porubsky1995@hotmail.com>
2022-01-19 00:39:56 +0100

    New headline updateable

--- newspage.php
+++ newspage.php
@@ -5,7 +5,7 @@
 <body>
 <br><br><br>
     <?php //News Post wird angezeigt
-    include "nav.php";
+    //include "nav.php";
     include "user_admin_check.php";
     include "user_indicator.php";
     if (isset($_GET["newsID"])){
@@ -52,16 +52,28 @@
         //Veränderungen am News Post werden in der Datenbank hochgeladen
         if(isset($_POST["active"]))  
         {
+            if(isset($_POST["updateHeadline"]) && $_POST["updateHeadline"] != ""  && isset($_POST["update_c"]))
+            {
+                $ID = $_SESSION["news_ID"];
+                $newHeadline = test_input($_POST["updateHeadline"]);
+                $sql = "update news 
+                    set headline = '$newHeadline'
+                    where news_id = $ID";
+                $stmt = $db_obj->prepare($sql);
+                $stmt->execute();
+                $stmt->close();
+            }
             if(isset($_POST["content"]) && isset($_POST["update_c"]))
             { //Text und Active State(ob Post angezeigt wird) wird aktualisiert
                 $ID = $_SESSION["news_ID"];
+                $newContent = test_input($_POST["content"]);
                 $resolvInput = $_POST["active"];
                 $sql = "update news 
                         set active = $resolvInput,
                         content = ?
                         where news_id = $ID";
                 $stmt = $db_obj->prepare($sql);
-                $stmt->bind_param('s', $_POST["content"]);
+                $stmt->bind_param('s', $newContent);
             }
             else
             {//Nur Active State wird aktualisiert
@@ -82,7 +94,18 @@
             header("Refresh:0; url=newsVerwaltung.php");
          }
          else if(isset($_POST["update_c"]))
-         {  //Nur Text wird aktualisiert
+         { 
+            if(isset($_POST["updateHeadline"]) && $_POST["updateHeadline"] != "")
+            {
+                $ID = $_SESSION["news_ID"];
+                $newHeadline = test_input($_POST["updateHeadline"]);
+                $sql = "update news 
+                    set headline = '$newHeadline'
+                    where news_id = $ID";
+                $stmt = $db_obj->prepare($sql);
+                $stmt->execute();
+                $stmt->close();
+            } //Nur Text wird aktualisiert
             $ID = $_SESSION["news_ID"];
             $sql = "update news 
                     set content = ?
@@ -109,11 +132,15 @@
         <?php 
         if($_SESSION["SessionWert"] == "Admin"){ 
             if(isset($active)){
+                echo "<label class='form-label' for='updateHeadline'>Update Headline</label><br>
+                <input type='text' name='updateHeadline'><br>";
                 if(isset($content)){
-                    echo "<textarea placeholder='news-content' required name='content'>".$content."</textarea>";
+                    echo "<label class='form-label' for='content'>Update content</label><br>
+                    <textarea placeholder='news-content' required name='content'>".$content."</textarea>";
                 }
                 else{
-                    echo "<textarea placeholder='news-content' required name='content'></textarea>";
+                    echo "<label class='form-label' for='content'>Update content</label><br>
+                    <textarea placeholder='news-content' required name='content'></textarea>";
                 }
                 if(isset($content) && $active == 1){
                     echo "<br><input name='active' type='radio' value=0 >Don't Display";
